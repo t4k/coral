6dba9c5e093c1c2cb87bce9fff3427a1a8ab75ac
Luke Aeschleman <lukeaeschleman@gmail.com>
2020-08-08 13:13:35 -0400

    add missing yop to r5 attribute mapper

--- usage/admin/classes/domain/SushiService.php
+++ usage/admin/classes/domain/SushiService.php
@@ -1103,6 +1103,7 @@ class SushiService extends DatabaseObject
       'Print_ISSN' => 'issn',
       'Online_ISSN' => 'eissn',
       'URI' => 'uri',
+      'YOP' => 'yop', 
       'Section_Type' => 'sectionType',
       'Access_Type' => 'accessType',
       'Publication_Date' => 'publicationDate',
